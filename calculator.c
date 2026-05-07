eb53bb12d236f55a236ba6dc0e3531452d8bd361
NyxieLucy <nyxlucianis@gmail.com>
2026-05-07 23:21:46 +0100

    fixed the problems with the logic and the syntax

--- calculator.c
+++ calculator.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 int main(void){
-    char operator [1];
+    char operator;
 
     int x;
     int y;
     printf("what's your first number?: ");
     scanf("%i", &x);
     printf("what kinda operator do ya wanna use? (+,-,*,/): ");
-    scanf("%c", operator);
+    scanf(" %c", &operator);
     printf("what's your second number?: ");
     scanf("%i", &y);
-    if (operator == "+") {
-        int some = x + y;
-        printf("the some is: %i", some);
+    if (operator == '+') {
+        printf("the some is: %i", x+y);
     }
-    else if (operator == "-") {
-        int subs = x - y;
-        printf("the result is: %i", subs);
+    else if (operator == '-') {
+        printf("the result is: %i", x-y);
     }
-    else if (operator == "*") {
-        int mult = x * y;
-        printf("the result is: %i", mult);
+    else if (operator == '*') {
+        printf("the result is: %i", x*y);
     }
-    else if (operator == "/") {
-        int dev = x / y;
+    else if (operator == '/') {
         if (y != 0) {
-            printf("the result is: %i", dev);
+            printf("the result is: %i", x/y);
         }
         else {
             printf("you can't devide by 0");
         }
     }
-
-
 }
---
+++ notes_for_calculator.md
@@ -0,0 +1,15 @@
+## i figured it out 
+
+writing "+" isn't as writing '+'
+apparently it's like compairing strings and character or at least that's how i understand it
+
+### also 
+declaring new variables is just a waste of time and effort
+unstead of writing 
+``` sum = x + y; ```
+``` printf("your result is: %i", sum); ```
+just write:
+``` printf("ur result is %i", x+y); ```
+
+### update
+having ``` char operation [0] ``` was a bad idea, it prevents the code from showing output, i suppose it prevents the compiler from stocking the values in their places
