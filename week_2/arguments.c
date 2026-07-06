c6bc9f957ffd087657b9d8e68f7a2f6a0ed84028
NyxieLucy <nyxlucianis@gmail.com>
2026-07-06 15:28:48 +0100

    added the catsay asci printer

--- week_2/arguments.c
+++ week_2/arguments.c
@@ -2,6 +2,33 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  printf("hello %s\n", argv[1]);
+  // If the user didn't pass any text, give a default fallback message
+  if (argc < 2) {
+    printf("Usage: %s <message>\n", argv[0]);
+    return 1;
+  }
+
+  // 1. Print the speech bubble with the custom argument message
+  printf("\n  +");
+  for (int i = 0; i < strlen(argv[1]) + 2; i++)
+    printf("-");
+  printf("+\n");
+
+  printf("  | %s |\n", argv[1]);
+
+  printf("  +");
+  for (int i = 0; i < strlen(argv[1]) + 2; i++)
+    printf("-");
+  printf("+\n");
+
+  // 2. Print the majestic custom ASCII cat art with escaped backslashes
+  printf("   \\   /\\_____/\\ \n");
+  printf("    \\ /  o   o  \\ \n");
+  printf("     ( ==  ^  == ) \n");
+  printf("      )         ( \n");
+  printf("     (           ) \n");
+  printf("    ( (  )   (  ) ) \n");
+  printf("   (__(__)___(__)__)\n\n");
+
   return 0;
 }
